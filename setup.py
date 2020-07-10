476ab0964f16b49d9f933cfc13e53959ac63d60b
ecrespo66 <enrique.crespo.debenito@gmail.com>
2020-07-10 22:39:32 +0200

    v.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 
 setup(
-    name='iBot',
+    name='iBotAutomation',
     packages=['iBot'],  # Mismo nombre que en la estructura de carpetas de arriba
     version='0.3',
     license='LGPL v3',  # La licencia que tenga tu paquete
