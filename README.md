f4b388dfdc9a0179cd6090f897ddf44455fb8e52
ecrespo66 <enrique.crespo.debenito@gmail.com>
2020-07-14 11:20:18 +0200

    v.5

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # iBot-Automation
 iBot Automation is an RPA tool designed to Automate Business process.
 
-#Modules Available
-01. Browser - Automate Browser Activites
-02. DataBase - Create manage and modify databases
-03. Email - Read and send Emails
-04. Excel - Create, read and modify Excel workbooks
-05. Files - Work woith Files, folders, images and PDFs
-06. OCR - Convert images to text
-07. Robot -  Manage Robots, Queues and items
-08. System - Work with process and system activities
-09. Word - Create, edit and modify Word documents
+##Modules Available
+01. Browser - Automate Browser Activites.
+02. DataBase - Create manage and modify databases.
+03. Email - Read and send Emails.
+04. Excel - Create, read and modify Excel workbooks.
+05. Files - Work with Files, folders, images and PDFs.
+06. OCR - Convert images to text.
+07. Robot -  Manage Robots, Queues and items.
+08. System - Work with process and system activities.
+09. Word - Create, edit and modify Word documents.
+10. Screen - Find bitmap elements on screen, mouse and keyboard control (move, drag, click and type). 
 
 
 
--- iBot/screen_activities.py
+++ iBot/screen_activities.py
@@ -9,25 +9,26 @@ class Screen:
         self.mousePosition = pyautogui.position()
 
     @staticmethod
-    def FindElement(self, image):
+    def FindElement(image):
         button = pyautogui.locateCenterOnScreen(image)
+        if subprocess.call("system_profiler SPDisplaysDataType | grep 'retina'", shell=True) == 0:
+            button = (button[0]/2, button[1]/2)
         return button
 
     @staticmethod
-    def moveMouseTo(self, pos):
+    def moveMouseTo(pos):
         pyautogui.moveTo(pos[0], pos[1])
 
     @staticmethod
-    def click_image(self, png_name, clicks=2):
-        if subprocess.call("system_profiler SPDisplaysDataType | grep 'retina'", shell=True) == 0:
-            x = x / 2
-            y = y / 2
+    def click_image(png_name, clicks=2):
         button = pyautogui.locateCenterOnScreen(png_name)
+        if subprocess.call("system_profiler SPDisplaysDataType | grep 'retina'", shell=True) == 0:
+            button = (button[0] / 2, button[1] / 2)
         pyautogui.click(button, clicks=clicks)
         return
 
     @staticmethod
-    def dragTo(self, to, button='left'):
+    def dragTo(to, button='left'):
         pyautogui.dragTo(to[0], to[1], button=button)
 
     @staticmethod
