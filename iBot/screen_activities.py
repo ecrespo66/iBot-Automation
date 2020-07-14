375f1d91516904033d63b00733a2bee2ec342f1a
ecrespo66 <enrique.crespo.debenito@gmail.com>
2020-07-14 11:01:41 +0200

    v.5

---
+++ iBot/screen_activities.py
@@ -0,0 +1,32 @@
+import pyautogui
+import subprocess
+
+
+class Screen:
+
+    def __init__(self):
+        self.screenSize = pyautogui.size()
+        self.mousePosition = pyautogui.position()
+
+    @staticmethod
+    def FindElement(self, image):
+        button = pyautogui.locateCenterOnScreen(image)
+        return button
+
+    @staticmethod
+    def moveMouseTo(self, pos):
+        pyautogui.moveTo(pos[0], pos[1])
+
+    @staticmethod
+    def click_image(self, png_name, clicks=2):
+        if subprocess.call("system_profiler SPDisplaysDataType | grep 'retina'", shell=True) == 0:
+            x = x / 2
+            y = y / 2
+        button = pyautogui.locateCenterOnScreen(png_name)
+        pyautogui.click(button, clicks=clicks)
+        return
+
+    @staticmethod
+    def write(text):
+        for t in text:
+            pyautogui.press(t)
