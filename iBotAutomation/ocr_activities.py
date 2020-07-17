beb4c94d2d5e595146f47e01526b45d9ee749fff
ecrespo66 <enrique.crespo.debenito@gmail.com>
2020-07-17 11:31:23 +0200

    v.5

--- iBotAutomation/ocr_activities.py
+++ iBotAutomation/ocr_activities.py
@@ -1,10 +1,15 @@
 import os
 import pytesseract
 from PIL import Image
+import re
+
+class OCR:
+    def __init__(self, path):
+        self.path = path
+
+    def ocr_getText(self, filePath):
+        pytesseract.pytesseract.tesseract_cmd = self.path
+        text = pytesseract.image_to_string(Image.open(filePath))
+        return text
 
 
-def ocr_getText(filename):
-    CurrentPath = os.path.dirname(__file__)
-    pytesseract.pytesseract.tesseract_cmd = CurrentPath + '/tesseract/4.1.1/bin/tesseract'
-    text = pytesseract.image_to_string(Image.open(filename))
-    return text
